05aa16e4e829b6019d2be35ca2efd71e94e47858
lhommenul <idlhommenul@gmail.com>
2021-09-03 15:28:08 +0200

    add goodies / tshirt / boutique

---
+++ src/components/boutique/Goodies.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+import ArtistCard from '@/components/ArtistCard.vue'
+export default {
+  name: 'Boutique',
+  components: {
+    ArtistCard
+  },
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+<style scoped>
+      
+</style>
