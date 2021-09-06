bfe0256b7492a882ba0dfd80ab8e2cabb591f284
lhommenul <idlhommenul@gmail.com>
2021-09-06 17:29:54 +0200

    update env variables

---
+++ src/views/Connection.vue
@@ -0,0 +1,30 @@
+<template>
+    <form action="/connection" method="">
+        <label>
+            UserName
+            <input type="text" placeholder="Username">
+        </label>
+        <label>
+            Password
+            <input type="password" placeholder="Password">
+        </label>
+        <input type="submit" value="connection">
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            yes:process.env.VUE_APP_SERVER_HOST
+        }
+    },
+    mounted() {
+        `${process.env.VUE_APP_SERVER_HOST}:${process.env.VUE_APP_SERVER_PORT}`
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
