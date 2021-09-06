b4992f28fd6f0f89e9effffe435c6c1082a53ef2
lhommenul <idlhommenul@gmail.com>
2021-09-06 16:06:19 +0200

    add authentification folder

--- server/auth.js
+++ server/auth.js
@@ -1,20 +1,34 @@
 let jwt = require('jsonwebtoken');
+
 require('dotenv').config();
 
+const setToken = (user_name = String,user_first_name= String,user_email= String)=>{
+    return jwt.sign({
+        name:user_name,
+        first_name:user_first_name,
+        email:user_email
+    }, process.env.SECRET_TOKEN);
+}
 
-const user_informations = { foo: 'bar' };
-
-let token = jwt.sign(user_informations, process.env.SECRET_TOKEN);
-
-
-const auth = (req, res, next)=>{
-
+const checkToken = (req, res, next)=>{
+    if (req.cookies.rv_token) {
+        jwt.verify(req.cookies.rv_token, process.env.SECRET_TOKEN, function(err, decoded_token) {
+            if (err) {
+                throw err;
+            } else {
+                console.log(decoded_token);
+            }
+        });
+    } else {
+        console.error("There is no token");
+    }
+    next(); // Must be changed for the prod
     console.log(`Current time is ${new Date().getTime()}`)
-    
-    next();
-    
 }
 
 
-module.exports = {auth}
+module.exports = {
+    checkToken,
+    setToken
+}
 
--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 const router = require("./route");
-const {auth} = require('./auth');
+const cookieParser = require('cookie-parser');
+const {checkToken} = require('./auth');
 
 require('dotenv').config(); // Env files
+
+app.use(cookieParser());
 
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 app.use(express.json());
 
-app.use(auth)
+app.use(checkToken)
 
 app.use(router);
 
